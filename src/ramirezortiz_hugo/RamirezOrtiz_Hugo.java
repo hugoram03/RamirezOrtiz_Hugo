46b524ec470a616114badb4a4e74ce851b277484
Hugo Ramirez Ortiz <huramsim@gmail.com>
2023-05-24 11:29:31 +0200

    1

---
+++ src/ramirezortiz_hugo/RamirezOrtiz_Hugo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ramirezortiz_hugo;
+
+/**
+ *
+ * @author Alumno
+ */
+public class RamirezOrtiz_Hugo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
